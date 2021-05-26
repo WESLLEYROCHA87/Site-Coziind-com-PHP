46853544cd8cd550a42e3a7eb472a805f6045ad3
Weslley Rocha <48062297+WESLLEYROCHA87@users.noreply.github.com>
2021-05-26 16:24:15 -0300

    Criado Link externo para header e footer
    Nesta etapa foi criada página separada do header e do footer.

---
+++ footer.php
@@ -0,0 +1,43 @@
+ <!-- Rodapé -->
+ <footer class="container-fluid bg-rodape1">
+     <div class="container py-5">
+         <div class="row">
+             <div class="col-md-12">
+                 <div class="card bg-card-rodape">
+                     <div class="card-body">
+                         <h2 class="text-light text-center"> Endereço:</h2>
+                         <p class="text-light text-center mt-3"> Endereço: Rua Dr Quirino, 734 Edifício Portal do Carmo - Sala 61 Centro - Campinas-SP - CEP 13015-081 </p>
+                         <p class="text-light text-center mt-3"> Telefone: (19) 3232-2829 </p>
+                         <p class="text-light text-center mt-3"> E-mail: <a class="text-light" href="mailto:coziind@coziind.com.br" title=""> coziind@coziind.com.br</a></p>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </footer>
+
+ <!-- Row Final Site -->
+ <div class="container-fluid mt-0 py-0 bg-rowTopo">
+     <div class="row">
+         <div class="col-md-12">
+             <p class="text-center text-light my-2">Desenvolvido por <a class="text-light" href="https://www.linkedin.com/in/weslley-rocha/" target="_blank"> Weslley Rocha</a>&#174;
+                 <?php echo  date("Y", time()); ?>
+             </p>
+         </div>
+     </div>
+ </div>
+
+ <!-- Link Externo -->
+ <!-- <script src="js/jquery-3.6.0.min.js"></script> -->
+ <script src="js/jquery-min.js"></script>
+ <script src="js/popper.min.js"></script>
+ <script src="js/bootstrap.min.js"></script>
+ <script src="js/jquery.counterup.min.js"></script>
+ <script src="js/waypoints.min.js"></script>
+ <!--Script pessoal-->
+ <script type="text/javascript" src="js/main.js"></script>
+
+
+ </body>
+
+ </html>
---
+++ header.php
@@ -0,0 +1,65 @@
+<!-- Site desenvolvido por Weslley Rocha -->
+<!DOCTYPE html>
+<html lang="PT-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Coziind</title>
+
+    <!-- CSS Bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <!-- CSS personalizado -->
+    <link rel="stylesheet" href="css/estilo.css">
+    <!-- Fonte para Icones -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+    <link rel="shortcut icon" href="img/Logos/favicon.ico">
+
+</head>
+
+<body>
+
+    <!-- Row Topo Site -->
+    <div class="container-fluid mt-0 py-0 bg-rowTopo">
+        <div class="row">
+            <div class="col-md-12">
+                <p class="text-center text-light my-2">Especialista em Licitação Pública</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- Navegador -->
+    <div class="container-fluid bg-topo">
+        <header class="container">
+            <nav class="navbar navbar-expand-lg navbar-dark ">
+                <div class="container">
+                    <div class="row">
+                        <div class="col">
+                            <a href="#" class="navbar-brand">
+                                <img src="img/Logos/LogoCoziindCorBranco.png" height="120" class="d-inline-block align-top" alt="" title="Logo Coziind">
+                            </a>
+                        </div>
+                    </div>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu" aria-controls="menu" aria-expanded="false" aria-label="Menu Colapso">
+                        <span class="navbar-toggler-icon text-light"></span>
+                    </button>
+                </div>
+                <div class="container my-1">
+                    <div class="row">
+                        <div class="col">
+                            <div id="menu" class="collapse navbar-collapse">
+                                <ul class="navbar-nav mr-auto text-light">
+                                    <li class="nav-item"> <a href="index.php" class="nav-link text-light">Início</a></li>
+                                    <li class="nav-item"> <a href="servicos.php" class="nav-link text-light">Serviços</a></li>
+                                    <li class="nav-item"> <a href="produtos.php" class="nav-link text-light">Produtos</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <a href="contato.php" class="btn col-md-3 mb-2 btn-contato">Entrar em contato</a>
+            </nav>
+        </header>
+    </div>
